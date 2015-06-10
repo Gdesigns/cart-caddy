8a5b295b643976a74f9afd1a62d4579c4b6532a7
Greg Hoffman <greg@4walls.net>
2015-06-10 12:10:10 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){		
+	// Get text from form input and display on list
+	$(".add").on("click", function(){
+		event.preventDefault(); // lets the user hit "enter" instead of having to click the "+" block
+		var item = $("input[name=item]").val();
+	// If no letters are entered, an alert message will pop up
+	if (item <= 0) {
+		alert ("Please add an item")
+	} else {
+		$("#shopping-list").append("<li>" + "<a class=remove>x</a>" + " " + item + "</li>");
+		$("input[name=item]").val("");
+	}
+	})
+	// The following changes the item to completed
+	$("li").click(function() {$(this).toggleClass("completed");});
+	// The following removes the item from the list after it has been added
+	$("ul").on('click', '.remove', function() {$(this).closest("li").remove();});
+	// Resets the shopping list
+	$(".reset").click(function() {$("#shopping-list").empty();})
+}); 
